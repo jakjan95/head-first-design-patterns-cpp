9da4b5d26726e798978912b81caebd283ec23c9d
jakjan95 <jakubjanusz95@gmail.com>
2020-07-09 17:55:20 +0200

    Observer pattern initialy finished

--- observer/main.cpp
+++ observer/main.cpp
@@ -2,15 +2,26 @@
 
 #include "WeatherData.hpp"
 #include "CurrentConditionDisplay.hpp"
+#include "StatisticsDisplay.hpp"
+#include "ForecastDisplay.hpp"
+#include "HeatIndexDisplay.hpp"
 
 int main(){
-    std::cout<<"Hello!\n";
+    std::cout<<"Observer pattern:\n\n";
 
     WeatherData weatherData;
     CurrentConditionsDisplay currentConditionDisplay(weatherData);
+    StatisticsDisplay statisticsDisplay(weatherData);
+    ForecastDisplay forecastDisplay(weatherData);
+    HeatIndexDisplay heatIndexDisplay(weatherData);
 
-    weatherData.setMeasurments(19.3, 75, 1000);
+    weatherData.setMeasurments(26.6, 65, 1000);
     weatherData.setMeasurments(21.5, 65, 990);
+    weatherData.setMeasurments(25.5, 55, 899);
+    weatherData.setMeasurments(21.5, 85, 1010);
+    weatherData.setMeasurments(15.5, 65, 970);
+    weatherData.setMeasurments(16.5, 65, 970);
+    weatherData.setMeasurments(25.5, 90, 970);
 
     return 0;
 }
