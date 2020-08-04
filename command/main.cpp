420ba6082fef2ff8abcb11f370fabf5d1576b3a6
jakjan95 <jakubjanusz95@gmail.com>
2020-08-04 21:30:22 +0200

    Added classes into initial version of command pattern example - SimpleRemoteControl

--- command/main.cpp
+++ command/main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 
-
 #include "SimpleRemoteControl.hpp"
+
 #include "Light.hpp"
 #include "LightOnCommand.hpp"
 
-int main(){
+#include "GarageDoor.hpp"
+#include "GarageDoorOpenCommand.hpp"
+
+int main() {
     SimpleRemoteControl remote;
-    auto light = std::make_shared<Light>();
-    auto lightOn = std::make_shared<LightOnCommand>(light);
+    auto lightOn = std::make_shared<LightOnCommand>(std::make_shared<Light>());
+    auto garageOpen = std::make_shared<GarageDoorOpenCommand>(std::make_shared<GarageDoor>());
 
     remote.setCommand(lightOn);
+    remote.buttonWasPressed();
+
+    remote.setCommand(garageOpen);
     remote.buttonWasPressed();
 
     return 0;
